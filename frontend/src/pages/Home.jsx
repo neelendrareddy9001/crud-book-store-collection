09116f3e8a0aa06034abf56963e2f77b332b622f
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-02-27 21:04:44 +0100

    new splitting component added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+
 import Spinner from "../components/Spinner";
+import BookTable from "../components/home/BookTable";
+import BookCard from "../components/home/BookCard";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
 
   useEffect(() => {
     setLoading(true);
@@ -27,63 +27,26 @@ const Home = () => {
   return (
     <>
       <div className="p-4 bg-lime-300 m-40 rounded-2xl shadow-xl">
-        <div className="flex justify-between items-center">
-          <h1 className="text-3xl my-8 title mx-auto px-8">Book List</h1>
-          <Link to="/books/create">
-            <MdOutlineAddBox className="text-sky-800 text-4xl" />
-          </Link>
+        <div className="flex justify-center items-center gap-x-4">
+          <button
+            className="bg-sky-300 hover:bg-sky-600 p-8 py-1 font-semibold my-4 rounded-lg"
+            onClick={() => setShowType("table")}
+          >
+            Table
+          </button>
+          <button
+            className="bg-sky-300 hover:bg-sky-600 p-8 font-semibold py-1 my-4 rounded-lg"
+            onClick={() => setShowType("card")}
+          >
+            Card
+          </button>
         </div>
         {loading ? (
           <Spinner />
+        ) : showType === "table" ? (
+          <BookTable books={books} />
         ) : (
-          <table className="w-full border-separate border-spacing-2">
-            <thead>
-              <tr>
-                <th className="border border-slate-600 rounded-md">No.</th>
-                <th className="border border-slate-600 rounded-md">Title</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">
-                  Author
-                </th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">
-                  PublishYear
-                </th>
-                <th className="border border-slate-600 rounded-md">
-                  Operations
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {books.map((book, index) => (
-                <tr key={book._id} className="h-8">
-                  <td className="border border-slate-700 rounded-md text-center">
-                    {index + 1}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center">
-                    {book.title}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                    {book.author}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                    {book.publishYear}
-                  </td>
-                  <td className="border border-slate-700 text-center">
-                    <div className="flex justify-center gap-x-4">
-                      <Link to={`/books/details/${book._id}`}>
-                        <BsInfoCircle className="text-2xl text-green-800" />
-                      </Link>
-                      <Link to={`/books/edit/${book._id}`}>
-                        <AiOutlineEdit className="text-2xl text-yellow-600" />
-                      </Link>
-                      <Link to={`/books/delete/${book._id}`}>
-                        <MdOutlineDelete className="text-2xl text-red-600" />
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <BookCard books={books} />
         )}
       </div>
     </>
